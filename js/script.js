65dcd6bf7a88b3d12291117a37aaf15abce10482
Vinicius Figueira <figueiravinicius@hotmail.com>
2024-10-19 15:12:18 -0300

    Fetch bitcoin

--- js/script.js
+++ js/script.js
@@ -8,14 +8,16 @@ import initDropdownMenu from "./modules/dropdown-menu.js"
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionanamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
-initModal()
-initTooltip()
-initDropdownMenu()
+initModal();
+initTooltip();
+initDropdownMenu();
 initMenuMobile();
-initFuncionanamento()
-initFetchAnimais()
+initFuncionanamento();
+initFetchAnimais();
+initFetchBitcoin();
